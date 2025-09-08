8c9e9f4f9d301b109be3df105bed719df6ff4603
arthur <arthur.s.vale@gmail.com>
2025-09-08 11:28:35 -0300

    Criado a seção do header e realizado o ajuste para mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const question = document.querySelectorAll('[data-faq-question]');
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function () {
+        const posicaoAtual = this.window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosHeader();
+        } else {
+            exibeElementosHeader();
+        }
+    })
 
     // Seção de atrações, programação de abas
     for (let i = 0; i < buttons.length; i++){
@@ -19,6 +31,16 @@ document.addEventListener('DOMContentLoaded', function () {
         question[i].addEventListener('click', abreOuFechaResposta);
     }
 })
+
+function ocultaElementosHeader() {
+    const header = document.querySelector('.header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosHeader() {
+    const header = document.querySelector('.header');
+    header.classList.remove('header--is-hidden');
+}
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
